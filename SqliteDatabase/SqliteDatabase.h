f56779860fd347c6b38c80312e3c961c7944206a
Domagoj Kulundžić <domagoj.kulundzic@farmeron.com>
2017-02-03 13:56:38 +0100

    Initial commit.

---
+++ SqliteDatabase/SqliteDatabase.h
@@ -0,0 +1,19 @@
+//
+//  SqliteDatabase.h
+//  SqliteDatabase
+//
+//  Created by Domagoj Kulundžić on 03/02/17.
+//  Copyright © 2017 Farmeron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SqliteDatabase.
+FOUNDATION_EXPORT double SqliteDatabaseVersionNumber;
+
+//! Project version string for SqliteDatabase.
+FOUNDATION_EXPORT const unsigned char SqliteDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SqliteDatabase/PublicHeader.h>
+
+
